9aea372e49928596f6fe95df2ee8ebb07ca06aa2
Thomas Corbett <thomascorbett@Thomass-MacBook-Pro.local>
2017-03-14 23:31:37 -0400

    first commit

---
+++ src/Omnivore.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tomcorbett\Omnivore;
+
+class Omnivore
+{
+    protected $apiKey;
+    protected $apiSecret;
+
+    public static const ID_BOOM = "GET FROM HERE";
+
+    public function __construct($configKey, $configSecret)
+    {
+        var_dump("constructor!!");
+        $this->apiKey     = $configKey;
+        $this->apiSecret  = $configSecret;
+    }
+
+    public function boom()
+    {
+        echo "YEAH!";
+        echo $this->apiSecret;
+        echo $this->apiKey;
+    }
+}
