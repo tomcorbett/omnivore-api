4d8bf2c012452223d2f1b024083a33cd77141e74
Thomas Corbett <thomascorbett@Thomass-MacBook-Pro.local>
2017-03-19 14:21:08 -0400

    added guzzle

---
+++ src/Omnivore/Request/Abstract.php
@@ -0,0 +1,13 @@
+<?php
+class RequestAbstract {
+
+    const API_BASE_URL = 'https://api.omnivore.io/1.0/';
+
+    protected $methodName = 'locations';
+
+    public function makeRequest()
+    {
+        //$this->
+    }
+
+}
